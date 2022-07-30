4f96d13c58793570698006175ccdae11b6586fd6
Guruprasaath <96716307+AEGONN-TARGARYEN@users.noreply.github.com>
2022-07-30 22:02:01 +0530

    Thathuvam

---
+++ main.py
@@ -0,0 +1,111 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(name)  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('''Amuku dumuku amaal dumaal
+
+Amuku dumuku amaal dumaal
+Hey jala bula jangu
+Hey dama duma dangu
+Thug life la kingu
+Don settingu
+Hey jinukunaa jingu
+Our age yo youngu
+Annal parakanum pangu
+Un fittingu
+
+Aavo bhai dosta aayega aayega
+Jolly mela maza aayega
+Bad vibeu jaldi jaayega jaayega
+Peace dhaan bro tension jaayega
+
+Freeya vudu collegea bunk adichalum
+Amuku dumuku
+Dean-kku dhaan paadam ettha
+needhaan paa donu
+Amuku dumuku
+Chilla vudu
+jalukku jalukku jalukku
+Arrear adhugam unaku
+But life la jeicha
+Needhaan paa donu
+
+Freeya vudu kai kitta suthudhu funnu
+Kattipuchu confirmaa
+Enjoy ya pannu
+Chilla vudu
+jalukku jalukku jalukku
+Puchikka podhu sulukku
+Staffsu ellam
+Dandanaa dannu
+Amuku dumuku amaal dumaal
+
+Hey jala bula jangu
+Hey dama duma dangu
+Thug life la kingu
+Don settingu
+Hey jinukunaa jingu
+Our age yo youngu
+Annal parakanum pangu
+Un fittingu
+
+Ozhugethaadha kaduppethadha
+Serious pesi nee sirippethaadha
+Kadha oatadha
+Padam kaatadha
+Vaanaambaa last bencha
+Veruppethaadha
+Dama dama dama dama dama
+Damukku dampa dammapa
+Gala gala galanurukkurom
+Unakku ennappa
+Para para para paranu
+Sendhuvom onnaappaa
+Morchanaa therikum yappappa
+
+Freeya vudu collegea bunk adichalum
+Dean-kku dhaan paadam ettha
+needhaan paa donu
+Chilla vudu
+jalukku jalukku jalukku
+Arrear adhugam unaku
+But life la jeicha
+Needhaan paa donu
+
+Freeya vudu collegea bunk adichalum
+Amuku dumuku
+Dean-kku dhaan paadam ettha
+needhaan paa donu
+Amuku dumuku
+Chilla vudu
+jalukku jalukku jalukku
+Arrear adhugam unaku
+But life la jeicha
+Needhaan paa donu
+Amuku dumuku amaal dumaal
+
+Hey jala bula jangu
+Hey dama duma dangu
+Thug life la kingu
+Don settingu
+Hey jinukunaa jingu
+Our age yo youngu
+Annal parakanum pangu
+Un fittingu
+
+Aavo bhai dosta aayega aayega
+Aayega
+Bad vibeu jaldi jaayega jaayega
+Hey jaayega
+Amuku dumuku amaal dumaal''')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
